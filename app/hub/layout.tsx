a1c00a229e4b5068de072b3370ec28bd579d93c0
ernkeyz <exconzy26@gmail.com>
2024-02-22 21:02:26 +0000

    hub layout update

---
+++ app/hub/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navigation/navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="">
+      <div className="">
+        <Navbar />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
