798fa50243bdc785a9ef60497a231df7b9ca63d8
ernkeyz <exconzy26@gmail.com>
2024-02-22 21:01:39 +0000

    homelayout update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { lusitana } from "./ui/fonts";
+import { ModeToggle } from "@/components/ModeToggler/modeToggler";
+
+import { CardDemo } from "@/components/Header/CardDemo";
 import Navbar from "@/components/Navigation/navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
+      <ModeToggle />
+      <CardDemo />
     </main>
   );
 }
