5b4676ea1e6739c5b1a981b3809899f429dd4bda
ernkeyz <exconzy26@gmail.com>
2024-02-22 18:48:33 +0000

    navbar import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { lusitana } from "./ui/fonts";
+import Navbar from "@/components/Navigation/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-0">
+      <Navbar />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p
           className={`${lusitana.className} text-xl text-white-800 md:text-3xl md:leading-normal`}
