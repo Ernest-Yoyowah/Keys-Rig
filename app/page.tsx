a7740b19a72d2a2311224862e6d80fefe4f69477
ernkeyz <exconzy26@gmail.com>
2024-02-22 19:13:43 +0000

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,8 @@ import Navbar from "@/components/Navigation/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-0">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p
-          className={`${lusitana.className} text-xl text-white-800 md:text-3xl md:leading-normal`}
-        >
-          <strong>Welcome to Keys Rig.</strong> This is the Ultimate VST Hub
-        </p>
-      </div>
     </main>
   );
 }
