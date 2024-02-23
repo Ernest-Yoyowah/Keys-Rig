fc661c88a32e477dc38528321e906d0623b1b863
ernkeyz <exconzy26@gmail.com>
2024-02-23 22:24:30 +0000

    Header Page

---
+++ components/Header/header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const Header = () => {
+  return (
+    <div className="">
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Header;
