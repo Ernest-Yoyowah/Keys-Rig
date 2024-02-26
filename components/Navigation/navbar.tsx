4af0256b4119f0bfe8ba7e6505f00d630328ec6b
ernkeyz <exconzy26@gmail.com>
2024-02-26 00:07:28 +0000

    clsx for active links

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -52,7 +52,7 @@ export default function Navbar() {
                 <Link
                   href={item.path}
                   className={clsx(
-                    "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-0 md:px-3",
+                    "flex h-[38px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-0 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-0 md:px-3",
                     {
                       "bg-sky-100 text-blue-600": pathname === item.path,
                     }
