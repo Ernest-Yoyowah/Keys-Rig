d7701411a66f6a14782052c3cff21070366ae980
ernkeyz <exconzy26@gmail.com>
2024-02-27 21:50:40 +0000

    Importing the AppBgImg in the header page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/Header/header";
 import Navbar from "@/components/Navigation/navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
+      {/* <Header /> */}
     </main>
   );
 }
