0e4b13e731ebe679574514e438b53351e1605e91
ernkeyz <exconzy26@gmail.com>
2024-02-27 21:54:48 +0000

    Adjusting the header image

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
-      {/* <Header /> */}
+      <Header />
     </main>
   );
 }
