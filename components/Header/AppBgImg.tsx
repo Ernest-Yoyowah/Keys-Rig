f2aa899b62ff9068ef3016e02f9f320666c9adeb
ernkeyz <exconzy26@gmail.com>
2024-02-27 21:39:26 +0000

    creating the app background

---
+++ components/Header/AppBgImg.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Image from "next/image";
+import header_bg from "@/public/header_bg.jpg";
+
+const AppBgImg = () => {
+  return <div>AppBgImg</div>;
+};
+
+export default AppBgImg;
