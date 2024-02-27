3f829f10c6d24ec1773958012dc1d7ea35c1cfc3
ernkeyz <exconzy26@gmail.com>
2024-02-27 21:32:18 +0000

    creating header background

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,31 +1,9 @@
 import React from "react";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import Image from "next/image";
+import header_bg from "@/public/header_bg.jpg";
 
 const Header = () => {
-  return (
-    <div className="">
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default Header;
