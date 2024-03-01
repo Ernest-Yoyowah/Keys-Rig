f6224db1ced336c42b20821aaf0d53c6f1c2e497
ernkeyz <exconzy26@gmail.com>
2024-03-01 20:10:05 +0000

    Header Section Update

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import AppBgImg from "./AppBgImg";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="">
-      <div className="w-max absolute p-10 m-20 left-20 ">
+    <div className="flex justify-center items-center">
+      <div className="absolute text-center m-0">
         <h1 className="text-white font-bold text-5xl">Welcome to Keys Rig</h1>
         <p className="text-white font-bold text-2xl">
           Step into a world where creativity knows no bounds...
         </p>
+        <br />
+        <div className="space-x-2">
+          <Button
+            variant="secondary"
+            className="bg-white text-black hover:bg-black hover:text-white hover:border-black"
+          >
+            <Link href="/">Featured Plugins</Link>
+          </Button>
+          <Button className="bg-black text-white border border-white hover:bg-white hover:text-black hover:border-white">
+            <Link href="/login">Key Rig</Link>
+          </Button>
+        </div>
+        <br />
+        <br />
       </div>
       <AppBgImg />
     </div>
