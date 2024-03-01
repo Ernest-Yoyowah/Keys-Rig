76d971f6e635d59197706df6fea4cbe858f12a7f
ernkeyz <exconzy26@gmail.com>
2024-03-01 22:16:06 +0000

    Navbar

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -62,6 +62,8 @@ export default function Navbar() {
                 </Link>
               </li>
             ))}
+
+            {/* <ModeToggle /> */}
           </ul>
         </div>
       </div>
