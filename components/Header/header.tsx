d91499a5e28779568ca2199bd39945e82d48b80f
ernkeyz <exconzy26@gmail.com>
2024-03-01 19:01:57 +0000

    Header Section Update

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -4,6 +4,12 @@ import AppBgImg from "./AppBgImg";
 const Header = () => {
   return (
     <div className="">
+      <div className="w-max absolute p-10 m-20 left-20 ">
+        <h1 className="text-white font-bold text-5xl">Welcome to Keys Rig</h1>
+        <p className="text-white font-bold text-2xl">
+          Step into a world where creativity knows no bounds...
+        </p>
+      </div>
       <AppBgImg />
     </div>
   );
