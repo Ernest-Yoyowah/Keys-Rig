515d45db2101fc898658714b27df88b94d407a8d
ernkeyz <exconzy26@gmail.com>
2024-03-02 20:02:33 +0000

    Introductory Page

---
+++ components/Introductory/introductory.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+import Featured from "./featured";
+
+const Introductory: React.FC = () => {
+  return (
+    <>
+      <Featured />
+      <div className="py-12">
+        <div className="container mx-auto text-center">
+          <h2 className="text-3xl font-semibold mb-4 text-slate-700	">
+            Unlock Your Musical Potential
+          </h2>
+          <div className="max-w-3xl mx-auto">
+            <p className="text-lg text-gray-700">
+              Dive into the world of music production with our collection of
+              premium VST plugins for keys and synths. Whether you're a seasoned
+              musician or just starting out, our plugins offer unparalleled
+              sound quality and versatility, allowing you to create music that
+              truly stands out.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Introductory;
+
+{
+  /* <Image
+src="background-image.svg" // Replace with the path to your image
+alt="Music Production" // Replace with appropriate alt text
+width={400} // Adjust width as needed
+height={300} // Adjust height as needed
+/> */
+}
