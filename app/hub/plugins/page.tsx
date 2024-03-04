e2772dac20615afca53b915978435d28026aa990
ernkeyz <exconzy26@gmail.com>
2024-03-04 20:53:46 +0000

    vst plugins page - home layout

--- app/hub/plugins/page.tsx
+++ app/hub/plugins/page.tsx
@@ -1,9 +1,11 @@
+import Featured from "@/components/FeaturedRig/featuredRig";
 import React from "react";
 
 const Plugins = () => {
   return (
     <>
-      <h1>page</h1>
+      <h1>VST Plugins</h1>
+      <Featured />
     </>
   );
 };
