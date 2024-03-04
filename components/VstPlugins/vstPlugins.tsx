92e625e9b6b9440c566675fe628413cfa7dc1fac
ernkeyz <exconzy26@gmail.com>
2024-03-04 20:48:29 +0000

    vst page

--- components/VstPlugins/vstPlugins.tsx
+++ components/VstPlugins/vstPlugins.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Featured from "../FeaturedRig/featuredRig";
 
 const vstPlugins: React.FC = () => {
-  return <h1>vstPlugins</h1>;
+  return (
+    <>
+      <h1>vstPlugins</h1>
+      <Featured />
+    </>
+  );
 };
 
 export default vstPlugins;
