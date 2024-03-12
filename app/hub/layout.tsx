2d81c20aa03aaac06d1eac5b1dfe2b048f7402a6
ernkeyz <exconzy26@gmail.com>
2024-03-12 14:37:05 +0000

    hub layout - updating navbar import

--- app/hub/layout.tsx
+++ app/hub/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navigation/navbar";
+import { Navbar } from "@/components/Navigation/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
