2c3b75c3b4ff1c317c0d2f35615522563282ce53
ernkeyz <exconzy26@gmail.com>
2024-03-12 15:19:21 +0000

    updating navbar - added dropdown menu with links

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -2,70 +2,227 @@
 
 import * as React from "react";
 import Link from "next/link";
-import { Menu } from "lucide-react";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
 import { ModeToggle } from "../ModeToggler/modeToggler";
+import Image from "next/image";
 
-export default function Navbar() {
-  const [state, setState] = React.useState(false);
-  const pathname = usePathname();
+const keysRigComponents: {
+  title: string;
+  href: string;
+  description: string;
+}[] = [
+  {
+    title: "Main Keys",
+    href: "/hub/keysrig/main",
+    description: "Explore our main keyboard setup.",
+  },
+  {
+    title: "Tools Needed (Main Keys)",
+    href: "/hub/keysrig/main/tools",
+    description:
+      "Essential tools required for setting up the main keyboard rig.",
+  },
+  {
+    title: "Cables Needed (Main Keys)",
+    href: "/hub/keysrig/main/cables",
+    description: "Required cables for connecting the main keyboard rig.",
+  },
+  {
+    title: "Auxiliary Keys",
+    href: "/hub/keysrig/aux/",
+    description: "Discover our auxiliary keyboard setup.",
+  },
+  {
+    title: "Tools Needed (Auxiliary Keys)",
+    href: "/hub/keysrig/aux/tools",
+    description:
+      "Essential tools required for setting up the auxiliary keyboard rig.",
+  },
+  {
+    title: "Cables Needed (Auxiliary Keys)",
+    href: "/hub/keysrig/aux/cables",
+    description: "Required cables for connecting the auxiliary keyboard rig.",
+  },
+];
 
-  const menus = [
-    { title: "Home", path: "/" },
-    { title: "Plugins", path: "/hub/plugins" },
-    { title: "Keys Rig", path: "/hub/keysrig" },
-    { title: "Producer Tech Tips", path: "/hub/producerTech" },
-  ];
+const instrumentMarketComponents: {
+  title: string;
+  href: string;
+  description: string;
+}[] = [
+  {
+    title: "Buy and Sell Instruments",
+    href: "/hub/instrumentMarket/buy-sell",
+    description: "Explore the instrument market for buying, selling, and more.",
+  },
+  {
+    title: "Repair and Rental Services",
+    href: "/hub/instrumentMarket/repair-rental",
+    description:
+      "Find repair and rental services for your musical instruments.",
+  },
+  {
+    title: "Sound Engineering",
+    href: "/hub/instrumentMarket/sound-engineering",
+    description:
+      "Get assistance with sound engineering for your musical projects.",
+  },
+  {
+    title: "Meet Instrumentalists",
+    href: "/hub/instrumentMarket/meet-instrumentalists",
+    description:
+      "Connect with other instrumentalists and collaborate on musical projects.",
+  },
+];
 
+export function Navbar() {
   return (
     <nav className="bg-white w-full border-b md:border-0">
-      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
-          <Link href="/" className="flex">
-            <Image
-              src="/logo.jpg"
-              width={60}
-              height={5}
-              alt="Screenshots of the dashboard project showing desktop version"
-            />
-            <h1 className="text-1xl font-bold text-slate-800 mt-1">Keyz Rig</h1>
-          </Link>
-          <div className="md:hidden">
-            <button
-              className="text-slate-800 outline-none p-2  rounded-md focus:border-black-400 focus:border"
-              onClick={() => setState(!state)}
-            >
-              <Menu />
-            </button>
-          </div>
-        </div>
-        <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-            state ? "block" : "hidden"
-          }`}
-        >
-          <ul className="justify-center font-semibold items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-            {menus.map((item, idx) => (
-              <li key={idx} className="text-gray-600 hover:text-sky-700">
-                <Link
-                  href={item.path}
-                  className={clsx(
-                    "flex h-[38px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-0 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-0 md:px-3",
-                    {
-                      "bg-sky-100 text-blue-600": pathname === item.path,
-                    }
-                  )}
-                >
-                  {item.title}
+      <div className="max-w-screen-xl mx-auto md:flex md:px-8">
+        <div className="flex md:py-5 md:block">
+          <NavigationMenu>
+            <Link href="/" className="flex mr-10 ">
+              <Image
+                src="/logo.jpg"
+                width={60}
+                height={5}
+                alt="Screenshots of the dashboard project showing desktop version"
+              />
+              <h1 className="text-1xl font-bold text-slate-800 mt-1">
+                Keyz Rig
+              </h1>
+            </Link>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                  </NavigationMenuLink>
                 </Link>
-              </li>
-            ))}
-            {/* <ModeToggle /> */}
-          </ul>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Plugins</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                    <li className="row-span-3">
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                          href="/hub/plugins"
+                        >
+                          <Icons.logo className="h-6 w-6" />
+                          <div className="mb-2 mt-4 text-lg font-medium">
+                            vst plugins
+                          </div>
+                          <p className="text-sm leading-tight text-muted-foreground">
+                            Vst plugins crafted to elevate your music production
+                            experience.
+                          </p>
+                        </Link>
+                      </NavigationMenuLink>
+                    </li>
+                    <ListItem
+                      href="/hub/plugins/featured"
+                      title="Featured Plugins"
+                    >
+                      Upgrade your music production setup with exciting new
+                      virtual instruments
+                    </ListItem>
+                    <ListItem
+                      href="/hub/plugins/installation"
+                      title="Installation"
+                    >
+                      How to install and setup your vst plugins.
+                    </ListItem>
+                    <ListItem href="/hub/plugins/top" title="Most Used plugins">
+                      There are hundreds of free VSTs online, and finding good
+                      ones takes time.
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Keys Rig</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {keysRigComponents.map((component) => (
+                      <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                      >
+                        {component.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Instrument Market</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {instrumentMarketComponents.map((component) => (
+                      <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                      >
+                        {component.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              {/* <ModeToggle /> */}
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
       </div>
     </nav>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
