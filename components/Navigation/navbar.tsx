1ea145fbc0b32d7e8f6757a1df1df59009aa67a3
ernkeyz <exconzy26@gmail.com>
2024-03-12 22:09:11 +0000

    making navbar responsive

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -105,7 +105,7 @@ export function Navbar() {
               </h1>
             </Link>
 
-            <NavigationMenuList className="flex md:hidden">
+            <NavigationMenuList className="hidden md:blog">
               <NavigationMenuItem>
                 <Link href="/" legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
