0ea850408c71e9c4ebda96c73cbb19fd89d45419
ernkeyz <exconzy26@gmail.com>
2024-03-14 22:48:00 +0000

    updating mobile nav menu

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -4,10 +4,8 @@ const About: React.FC = () => {
   return (
     <>
       <div className="container mx-auto my-auto text-center py-8 mt-10 ">
-        <h1 className="text-3xl font-semibold mb-4 text-[#f5f5f5]">
-          About Keys Rig
-        </h1>
-        <div className="max-w-3xl mx-auto  text-[#f5f5f5]">
+        <h1 className="text-3xl font-semibold mb-4 ">About Keys Rig</h1>
+        <div className="max-w-3xl mx-auto  ">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
             dicta ex nulla, voluptas odio suscipit eveniet accusamus iste nisi
