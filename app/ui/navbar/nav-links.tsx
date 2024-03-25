e407d25d464cf83f45bfea9dd73aded89880a38c
ernkeyz <exconzy26@gmail.com>
2024-03-25 23:04:49 +0000

    changing the color of an active navlink

--- app/ui/navbar/nav-links.tsx
+++ app/ui/navbar/nav-links.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"; // Import useState
+"use client";
 import {
   UserGroupIcon,
   HomeIcon,
@@ -10,21 +10,100 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: "Home", href: "/", icon: HomeIcon },
-  { name: "Plugins", href: "/hub/plugins", icon: PuzzlePieceIcon },
-  { name: "Keys Rig", href: "/hub/keysrig", icon: CubeIcon },
+  {
+    name: "Plugins",
+    href: "/hub/plugins",
+    icon: PuzzlePieceIcon,
+  },
+  { name: "Keys Rig", href: "/hub/keysrig", icon: CubeIcon }, // Use CubeIcon for "Keys Rig"
+
   {
     name: "Instrument Market",
     href: "/hub/instrumentMarket",
-    icon: MusicalNoteIcon,
+    icon: MusicalNoteIcon, // Use MusicNoteIcon for "Instrument Market"
   },
   { name: "Blog", href: "/blog", icon: UserGroupIcon },
 ];
 
+export type KeysRigComponent = {
+  title: string;
+  href: string;
+  description: string;
+};
+
+export type InstrumentMarketComponent = {
+  title: string;
+  href: string;
+  description: string;
+};
+
+export const keysRigComponents: KeysRigComponent[] = [
+  {
+    title: "Main Keys",
+    href: "/hub/keysrig/main",
+    description: "Explore our main keyboard setup.",
+  },
+  {
+    title: "Tools Needed (Main Keys)",
+    href: "/hub/keysrig/main/tools",
+    description:
+      "Essential tools required for setting up the main keyboard rig.",
+  },
+  {
+    title: "Cables Needed (Main Keys)",
+    href: "/hub/keysrig/main/cables",
+    description: "Required cables for connecting the main keyboard rig.",
+  },
+  {
+    title: "Auxiliary Keys",
+    href: "/hub/keysrig/aux/",
+    description: "Discover our auxiliary keyboard setup.",
+  },
+  {
+    title: "Tools Needed (Auxiliary Keys)",
+    href: "/hub/keysrig/aux/tools",
+    description:
+      "Essential tools required for setting up the auxiliary keyboard rig.",
+  },
+  {
+    title: "Cables Needed (Auxiliary Keys)",
+    href: "/hub/keysrig/aux/cables",
+    description: "Required cables for connecting the auxiliary keyboard rig.",
+  },
+];
+
+export const instrumentMarketComponents: InstrumentMarketComponent[] = [
+  {
+    title: "Buy and Sell Instruments",
+    href: "/hub/instrumentMarket/buy-sell",
+    description: "Explore the instrument market for buying, selling, and more.",
+  },
+  {
+    title: "Repair and Rental Services",
+    href: "/hub/instrumentMarket/repair-rental",
+    description:
+      "Find repair and rental services for your musical instruments.",
+  },
+  {
+    title: "Sound Engineering",
+    href: "/hub/instrumentMarket/sound-engineering",
+    description:
+      "Get assistance with sound engineering for your musical projects.",
+  },
+  {
+    title: "Meet Instrumentalists",
+    href: "/hub/instrumentMarket/meet-instrumentalists",
+    description:
+      "Connect with other instrumentalists and collaborate on musical projects.",
+  },
+];
+
 export default function NavLinks() {
   const pathname = usePathname();
-  const [activeLink, setActiveLink] = useState(pathname); // Track active link
 
   return (
     <>
@@ -37,26 +116,42 @@ export default function NavLinks() {
             className={clsx(
               `flex h-[48px] grow items-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-white hover:text-black  md:p-2 md:px-3`,
               {
-                "text-black": pathname === link.href,
+                " text-[#a5a5a5]": pathname === link.href,
               }
             )}
-            // Set active link when link is clicked
-            onClick={() => setActiveLink(link.href)}
           >
-            {/* Apply color class based on active link */}
-            <LinkIcon
-              className={clsx("w-6", { "text-black": pathname === link.href })}
-            />
-            <p
-              className={clsx("block", {
-                "text-black": pathname === link.href,
-              })}
-            >
-              {link.name}
-            </p>
+            <LinkIcon className="text-[#a5a5a5] w-6" />
+            <p className="block">{link.name}</p>
           </Link>
         );
       })}
     </>
   );
 }
+
+// export default function NavLinks() {
+//   const pathname = usePathname();
+
+//   return (
+//     <>
+//       {links.map((link) => {
+//         const LinkIcon = link.icon;
+//         return (
+//           <Link
+//             key={link.name}
+//             href={link.href}
+//             className={clsx(
+//               `flex h-[48px] grow items-center  gap-2 text-[#f3eded] md:round p-3 text-sm font-medium hover:bg-white hover:text-black  md:p-2 md:px-3`,
+//               {
+//                 " text-black": pathname === link.href,
+//               }
+//             )}
+//           >
+//             <LinkIcon className=" text-[#a5a5a5] w-6" />
+//             <p className="block">{link.name}</p>
+//           </Link>
+//         );
+//       })}
+//     </>
+//   );
+// }
