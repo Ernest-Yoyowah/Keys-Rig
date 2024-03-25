874e4072e2f67b8f4f7be1f1b2d9a6e94464c117
ernkeyz <exconzy26@gmail.com>
2024-03-25 23:29:14 +0000

    adding metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog | Keys Rig",
+};
+
 const Blog = () => {
   return <div>Blog</div>;
 };
