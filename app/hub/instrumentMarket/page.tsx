3a0051f23cecaaa931a82108491229a20414a8ce
ernkeyz <exconzy26@gmail.com>
2024-03-25 23:35:04 +0000

    adding metadata

--- app/hub/instrumentMarket/page.tsx
+++ app/hub/instrumentMarket/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Buy and Sell | Keys Rig",
+};
+
 const ProduceTech: React.FC = () => {
   return <h1>ProduceTech</h1>;
 };
