bff069eee0a7c3766fd9666eb7bbcecf3953c7ed
ernkeyz <exconzy26@gmail.com>
2024-03-25 06:55:23 +0000

    updating the home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,14 @@ import { Navbar } from "@/components/Navigation/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Navbar />
-      <Header />
-      <Introductory />
-      <About />
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <Navbar />
+        <Header />
+        <Introductory />
+        <About />
+      </main>
+      <Footer />
+    </>
   );
 }
