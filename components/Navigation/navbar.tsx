d5c250a51666cc71e3f0a387cc71c7d38dffa8bc
ernkeyz <exconzy26@gmail.com>
2024-03-25 23:06:01 +0000

    changing the color of an active navlink

--- components/Navigation/navbar.tsx
+++ components/Navigation/navbar.tsx
@@ -42,7 +42,7 @@ export function Navbar() {
         onClick={closeMobileMenu}
       >
         <div className="bg-[#254d63] h-full w-64 transition-transform ease-in-out duration-300">
-          <div className="p-0 text-[#f3eded]">
+          <div className="p-0 text-[#aeaaaa]">
             <SideNav />
           </div>
         </div>
