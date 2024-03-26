0fbeb3cdc8f59bf2fd7594b5996d99f0ac66c45b
ernkeyz <exconzy26@gmail.com>
2024-03-26 13:13:42 +0000

    keys rig page

--- app/hub/plugins/page.tsx
+++ app/hub/plugins/page.tsx
@@ -1,6 +1,12 @@
 import Featured from "@/components/FeaturedRig/featuredRig";
 import React from "react";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Plugins | Keys Rig",
+};
+
 const Plugins = () => {
   return (
     <>
