3016825c76a471f68807220add4e3d5117cc0d0b
ernkeyz <exconzy26@gmail.com>
2024-03-27 18:22:56 +0000

    setting text alignment for mobile and desktop views

--- app/hub/keysrig/page.tsx
+++ app/hub/keysrig/page.tsx
@@ -39,13 +39,13 @@ const Rig: React.FC = () => {
 
   return (
     <div className="bg-[#172D44]">
-      <div className=" text-[#f5f5f5] flex md:justify-center md:items-center p-5 md:p-0">
+      <div className=" text-[#f5f5f5] flex md:justify-center md:items-center p-5 md:p-0 text-left	md:text-center">
         <div className=" ">
           <div className="max-w-5xl md:pb-6 md:pt-0 text-center space-y-10">
             <h1 className="text-2xl md:text-4xl font-bold mt-7 md:mt-20">
               Choose 9 Expansions. Pay only $99.00
             </h1>
-            <p className="pb-0 pt-0">
+            <p className="pb-0 pt-0 	">
               From soulful hip-hop to liquid drum and bass, or from lo-fi to
               classic house – whatever genre you’re making, there’s an Expansion
               to do it with. Until April 9, grab any nine Expansions for just
