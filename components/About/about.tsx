72ebca1e919a9ac124dfcf4e831204df181fd42c
ernkeyz <exconzy26@gmail.com>
2024-03-28 23:05:29 +0000

    about page update

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const About: React.FC = () => {
   return (
     <>
-      <div className="container mx-auto my-auto text-center py-8 mt-10 ">
-        <h1 className="text-3xl font-semibold mb-4 ">About Keys Rig</h1>
+      <div className="container mx-auto my-auto text-slate-800 md:text-center py-8 mt-10 ">
+        <h1 className="text-3xl font-semibold mb-4">About Keys Rig</h1>
         <div className="max-w-3xl mx-auto  ">
-          <p>
+          <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
             dicta ex nulla, voluptas odio suscipit eveniet accusamus iste nisi
             culpa rem non? Velit ipsam nihil optio enim fugit molestias atque!
