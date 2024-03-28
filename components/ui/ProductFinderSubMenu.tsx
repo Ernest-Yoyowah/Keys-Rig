334064e91d788344c1698f943202e2be022d664b
ernkeyz <exconzy26@gmail.com>
2024-03-28 23:39:06 +0000

    Product Finder Top Menu

---
+++ components/ui/ProductFinderSubMenu.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ProductFinderSubMenu: React.FC = () => {
+  return (
+    <ul className="text-center bg-gray-200 p-0">
+      <li className="product-finder-submenu-link mb-2">
+        <a href="/en/catalog/deals/" className="text-blue-600">
+          Deals
+        </a>
+      </li>
+      <li className="product-finder-submenu-link mb-2">
+        <a href="/en/catalog/free/" className="text-blue-600">
+          Free
+        </a>
+      </li>
+      <li className="product-finder-submenu-link mb-2">
+        <a href="/en/catalog/new/" className="text-blue-600">
+          New
+        </a>
+      </li>
+      <li className="product-finder-submenu-link mb-2">
+        <a href="/en/catalog/my-komplete-offers/" className="text-blue-600">
+          My Komplete offers
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default ProductFinderSubMenu;
