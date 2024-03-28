fa030ea9967150fdf14b98baf4a4736b14452675
ernkeyz <exconzy26@gmail.com>
2024-03-28 23:43:54 +0000

    Product Finder Top Menu

--- components/ui/ProductFinderTopMenu.tsx
+++ components/ui/ProductFinderTopMenu.tsx
@@ -8,19 +8,19 @@ const ProductFinderTopMenu: React.FC = () => {
     >
       <ul className="flex flex-wrap gap-x-4">
         <li>
-          <a href="/en/catalog/" className="text-white">
+          <a href="/en/catalog/" className="text-[#f0f0f0]">
             <span className="font-bold">OVERVIEW</span>
             <span className="block text-sm">All Products</span>
           </a>
         </li>
         <li>
-          <a href="/en/catalog/software/" className="text-white">
+          <a href="/en/catalog/software/" className="text-[#f0f0f0]">
             <span className="font-bold">SOFTWARE & SOUNDS</span>
             <span className="block text-sm">KOMPLETE, NKS, iZotope</span>
           </a>
         </li>
         <li>
-          <a href="/en/catalog/hardware/" className="text-white">
+          <a href="/en/catalog/hardware/" className="text-[#f0f0f0]">
             <span className="font-bold">HARDWARE</span>
             <span className="block text-sm">
               Keyboards, MASCHINE, TRAKTOR & more
@@ -28,19 +28,22 @@ const ProductFinderTopMenu: React.FC = () => {
           </a>
         </li>
         <li>
-          <a href="/en/catalog/9-for-99-special/" className="text-white">
+          <a href="/en/catalog/9-for-99-special/" className="text-[#f0f0f0]">
             <span className="font-bold">9 for 99</span>
             <span className="block text-sm">Mar 20 - Apr 9</span>
           </a>
         </li>
         <li>
-          <a href="/en/catalog/projectsam-offer-2024/" className="text-white">
+          <a
+            href="/en/catalog/projectsam-offer-2024/"
+            className="text-[#f0f0f0]"
+          >
             <span className="font-bold">NKS special</span>
             <span className="block text-sm">Mar 11 - Mar 28</span>
           </a>
         </li>
         <li>
-          <a href="/en/catalog/piano-day-offer/" className="text-white">
+          <a href="/en/catalog/piano-day-offer/" className="text-[#f0f0f0]">
             <span className="font-bold">Piano Day special</span>
             <span className="block text-sm">Mar 27 - Apr 2</span>
           </a>
