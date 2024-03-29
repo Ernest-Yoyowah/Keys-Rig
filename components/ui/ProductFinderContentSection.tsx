10c21393a4718d4a36a0f70797d1518768563926
ernkeyz <exconzy26@gmail.com>
2024-03-29 18:01:35 +0000

    Product Finder Content Section - vst plugins page

--- components/ui/ProductFinderContentSection.tsx
+++ components/ui/ProductFinderContentSection.tsx
@@ -11,7 +11,7 @@ const ProductFinderContentSection: React.FC = () => {
           New products
         </h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {/* Featured Product 1 */}
         <div className="product-tile-wrapper bg-white shadow-md rounded-lg p-4">
           <div className="relative">
