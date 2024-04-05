60111e44f78e37d6e6b0193d0aef42aabb749734
ernkeyz <exconzy26@gmail.com>
2024-04-05 22:43:28 +0000

    dummy data

---
+++ app/lib/productsData.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: "Kontrol S88",
+    description:
+      "Keyboard controller with 88 fully weighted hammer-action keys, deep software integration, and polyphonic aftertouch.",
+    price: 99.0,
+    imageUrl:
+      "https://www.native-instruments.com/typo3temp/pics/img-packshot-kontrol-s88-product-finder-567d15708f21fecf897559368e397a50-d.jpg",
+  },
+  {
+    id: 2,
+    name: "Korg Nano Control",
+    description:
+      "Keyboard controller with 88 fully weighted hammer-action keys, deep software integration, and polyphonic aftertouch.",
+    price: 74.0,
+    imageUrl:
+      "https://www.native-instruments.com/typo3temp/pics/img-packshot-kontrol-s88-product-finder-567d15708f21fecf897559368e397a50-d.jpg",
+  },
+];
