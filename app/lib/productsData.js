3f5cef8de98eed2f828773a92251e6898a306f4a
ernkeyz <exconzy26@gmail.com>
2024-04-10 20:17:38 +0000

    update

--- app/lib/productsData.js
+++ app/lib/productsData.js
@@ -10,9 +10,9 @@ export const products = [
   },
   {
     id: 2,
-    name: "Korg Nano Control",
+    name: "Session Bassist – Upright Bass",
     description:
-      "Keyboard controller with 88 fully weighted hammer-action keys, deep software integration, and polyphonic aftertouch.",
+      "The deep, organic low end of a stunning upright bass handcrafted in Mittenwald in the 1970s.",
     price: 74.0,
     imageUrl:
       "https://www.native-instruments.com/typo3temp/pics/img-packshot-prime-bass-product-finder-791eb4feaf8fe699eca2d9779a180e10-d.jpg",
