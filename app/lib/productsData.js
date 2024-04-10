46a21ccb1c6ddca053e0bf1c4848a396fa1368a5
ernkeyz <exconzy26@gmail.com>
2024-04-10 20:13:44 +0000

    update

--- app/lib/productsData.js
+++ app/lib/productsData.js
@@ -15,6 +15,6 @@ export const products = [
       "Keyboard controller with 88 fully weighted hammer-action keys, deep software integration, and polyphonic aftertouch.",
     price: 74.0,
     imageUrl:
-      "https://www.native-instruments.com/typo3temp/pics/img-packshot-kontrol-s88-product-finder-567d15708f21fecf897559368e397a50-d.jpg",
+      "https://www.native-instruments.com/typo3temp/pics/img-packshot-prime-bass-product-finder-791eb4feaf8fe699eca2d9779a180e10-d.jpg",
   },
 ];
