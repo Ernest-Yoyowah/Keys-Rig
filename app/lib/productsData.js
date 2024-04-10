aa758e0f2a57330e3b538bdbd132df0bee799cab
ernkeyz <exconzy26@gmail.com>
2024-04-10 20:20:18 +0000

    update

--- app/lib/productsData.js
+++ app/lib/productsData.js
@@ -26,4 +26,13 @@ export const products = [
     imageUrl:
       "https://www.native-instruments.com/typo3temp/pics/img-packshot-action-woodwinds-product-finder-86954294689a6dcd54fe6e3d9d5d5f0f-d.jpg",
   },
+  {
+    id: 4,
+    name: "Guitar Rig 7 Pro",
+    description:
+      "A multi-effects rack and amp simulator with cutting-edge machine learning technology and an intuitive interface.",
+    price: 99.0,
+    imageUrl:
+      "https://www.native-instruments.com/typo3temp/pics/img-packshot-guitar-rig-7-product-finder-2-a291f0dee4ca0dc08da0e04bc563ee3f-d.jpg",
+  },
 ];
