4886143d29c489f380de04eaf6ac19c62165a427
ernkeyz <exconzy26@gmail.com>
2024-04-16 18:24:45 +0000

    update

--- app/hub/keysrig/tools/page.tsx
+++ app/hub/keysrig/tools/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const GearPage = () => {
+  return (
+    <div className="relative h-96">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage:
+            'url("https://multitracks.blob.core.windows.net/public/images/products/sounds/ssp-video-bg.png")',
+        }}
+      ></div>
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="container mx-auto my-auto text-white text-center relative z-10 py-16">
+        <h1 className="text-3xl md:text-5xl font-semibold mt-20">
+          Keys Rig Rundown: Gear
+        </h1>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default GearPage;
