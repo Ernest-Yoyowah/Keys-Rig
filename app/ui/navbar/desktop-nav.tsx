77aba487259c93c226e09c3b96000409aa4a22f7
ernkeyz <exconzy26@gmail.com>
2024-04-17 23:11:14 +0000

    update

--- app/ui/navbar/desktop-nav.tsx
+++ app/ui/navbar/desktop-nav.tsx
@@ -49,6 +49,20 @@ const DesktopNav = () => {
                 Keys Rig
               </NavigationMenuLink>
             </Link> */}
+            <NavigationMenuTrigger>Keys Rig</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[780px] ">
+                {instrumentMarketComponents.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Instrument Market</NavigationMenuTrigger>
