4bb2b5681dabbd6e884d7e32a54b4ceac4796bf0
ernkeyz <exconzy26@gmail.com>
2024-04-18 19:14:22 +0000

    update- keys rig page

--- app/hub/keysrig/build-rig/page.tsx
+++ app/hub/keysrig/build-rig/page.tsx
@@ -4,14 +4,14 @@ const GearPage = () => {
   return (
     <div className="relative h-96">
       <div
-        className="absolute inset-0 bg-cover bg-center"
+        className="absolute  bg-cover bg-center"
         style={{
           backgroundImage:
             "https://worshipteamresources.com/wp-content/uploads/2021/11/1920x1080-KeyStudio-on-MacBook.png",
         }}
       ></div>
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="container mx-auto my-auto text-white text-center relative z-10 py-16">
+      <div className="container mx-auto my-auto text-white text-center relative py-16">
         <h1 className="text-3xl md:text-5xl font-semibold mt-20">
           The Ultimate Keyboard Utility Rig
         </h1>
