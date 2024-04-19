8f626b93ce6ac825a8807bcecee5388f89a90c9f
ernkeyz <exconzy26@gmail.com>
2024-04-19 22:02:00 +0000

    updating all pages

---
+++ components/ui/VideoCard.tsx
@@ -0,0 +1,24 @@
+import { Video } from "@/types/types";
+import React from "react";
+
+const VideoCard: React.FC<{ video: Video }> = ({ video }) => {
+  return (
+    <div className="rounded-md overflow-hidden shadow-md">
+      <iframe
+        width="100%"
+        height="240"
+        src={video.url}
+        title={video.name}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      <div className="p-4">
+        <h2 className="text-lg font-semibold mb-2">{video.name}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
