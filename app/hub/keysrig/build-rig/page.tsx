09703aa5ccd4a6a2879d9df4e9894cb3875c7e2c
ernkeyz <exconzy26@gmail.com>
2024-04-19 14:05:43 +0000

    update- keys rig page

--- app/hub/keysrig/build-rig/page.tsx
+++ app/hub/keysrig/build-rig/page.tsx
@@ -18,6 +18,48 @@ const BuildRigPage = () => {
           </h1>
         </div>
       </div>
+
+      <div className="md:mx-auto px-5 my-auto text-slate-800 md:text-center py-8">
+        <div className="max-w-3xl mx-auto">
+          <p>
+            One of the hardest parts of being a worship keys player is choosing
+            which gear to buy when building your keyboard rig.
+          </p>
+
+          <br />
+          <p>
+            Knowing what keys gear is right for you and how it works together
+            will benefit your playing, as well as the entire sound of the band!
+          </p>
+
+          <br />
+          <p>
+            In this 2 part video series, Keith Duell breaks down his keyboard
+            rig in full detail!
+          </p>
+
+          <div className="my-10 px-10 md:h-[456px] aspect-w-16 aspect-h-9">
+            <iframe
+              width="810"
+              height="456"
+              src="https://www.youtube.com/embed/neNbVMelMcQ"
+              title="Worship Rig Tour - Piano &amp; Keys Gear | Sunday Keys App Setup"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              className="w-full h-full"
+            ></iframe>
+          </div>
+
+          <p>
+            Keith has been performing in live worship bands and in the studio as
+            a keyboardist for many years. In this series, he will break down all
+            of the hardware and software required to achieve full, rich keys and
+            synth sounds. Watch now!
+          </p>
+        </div>
+      </div>
     </>
   );
 };
