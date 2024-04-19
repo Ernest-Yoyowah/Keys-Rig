7c613514694c4482ad60ab2df43ec388e879ef36
ernkeyz <exconzy26@gmail.com>
2024-04-19 13:38:20 +0000

    update- keys rig page

--- components/ui/ExpansionItem.tsx
+++ components/ui/ExpansionItem.tsx
@@ -11,22 +11,7 @@ const ExpansionItem: React.FC<ExpansionItemProps> = ({
     <div className="">
       <h2 className="text-white md:text-4xl font-bold md:mb-4">{title}</h2>
       <div className="text-white py-2 md:mb-6">{description}</div>
-      <div className="media media--youtube media--complex media--small media--padding-bottom flex text-[#fff]">
-        <a
-          href={youtubeLink}
-          className="media__link"
-          title="Watch now"
-          data-type="video"
-          data-content="7181"
-        >
-          <div className="media__thumb w-20">
-            <span className="icon icon--small">
-              <PlayCircleIcon />
-            </span>
-          </div>
-          <div className="media__text">Watch now</div>
-        </a>
-      </div>
+      <div className="media media--youtube media--complex media--small media--padding-bottom flex text-[#fff]"></div>
     </div>
   );
 };
