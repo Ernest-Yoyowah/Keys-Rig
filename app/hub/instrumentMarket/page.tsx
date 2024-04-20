3d9d6de075976a73899e4cd3b5a90434e7c61c3c
ernkeyz <exconzy26@gmail.com>
2024-04-20 22:01:37 +0000

    updating the keys rig page and its child components

--- app/hub/instrumentMarket/page.tsx
+++ app/hub/instrumentMarket/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Buy and Sell | Keys Rig",
+  title: "Buy and Sell - meet and collaborate with other musicians",
 };
 
 const ProduceTech: React.FC = () => {
