45534328cd37dcbb46ab4b09b44740e3acd30c8e
ernkeyz <exconzy26@gmail.com>
2024-04-21 23:56:05 +0000

    blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -23,7 +23,6 @@ const BlogPage: NextPage = () => {
         </div>
       </div>
 
-      {/* Main Content */}
       <div className="container mx-auto py-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="bg-white rounded-lg overflow-hidden shadow-lg">
