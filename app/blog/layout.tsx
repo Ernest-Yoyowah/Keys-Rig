f890a695da433f67cfc1d37cf5b9fad45f038d7d
ernkeyz <exconzy26@gmail.com>
2024-04-21 23:58:47 +0000

    blog page

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="">
         <Navbar />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-0">{children}</div>
+      <div>{children}</div>
     </>
   );
 }
