7db274360538ad8d7636d920702c0d4f4d48ff89
ernkeyz <exconzy26@gmail.com>
2024-04-21 23:57:24 +0000

    blog page

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -2,11 +2,11 @@ import { Navbar } from "@/components/Navigation/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="">
+    <>
       <div className="">
         <Navbar />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-0">{children}</div>
-    </div>
+    </>
   );
 }
