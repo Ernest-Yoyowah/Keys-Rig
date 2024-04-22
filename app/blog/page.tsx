a374bbdf354247731eb5a910f04ed7ea38bba8d2
ernkeyz <exconzy26@gmail.com>
2024-04-22 00:50:23 +0000

    blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -20,8 +20,8 @@ const largeBlogData = [
       "https://media.gettyimages.com/id/1352603244/photo/shot-of-an-unrecognizable-businessman-working-on-his-laptop-in-the-office.jpg?s=612x612&w=gi&k=20&c=P5z85b4Wg80UO19u2YvRAbmpwxqWyyImihiMb6gWp8I=",
   },
   {
-    title: "Large Blog Post Title 1",
-    author: "John Doe",
+    title: "Large Blog Post Title 2",
+    author: "Ernest Nii",
     date: "April 22, 2024",
     excerpt:
       "This is an example blog post excerpt for the large blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet lorem eu mauris fermentum malesuada. Aliquam posuere auctor justo, ac sagittis enim interdum in.",
@@ -37,7 +37,7 @@ const smallBlogData = [
     excerpt:
       "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     image:
-      "https://www.searchenginejournal.com/wp-content/uploads/2020/08/7-ways-a-blog-can-help-your-business-right-now-5f3c06b9eb24e-1280x720.png",
+      "https://github.blog/wp-content/uploads/2024/04/keyboard-shortcuts.png?resize=800%2C425",
   },
   {
     title: "Small Blog Post Title",
