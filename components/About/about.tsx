1d8f04682c9e083c7151eab9a5fab28dd512917d
ernkeyz <exconzy26@gmail.com>
2024-04-23 11:20:32 +0000

    updating the typography for consistency

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -4,9 +4,9 @@ const About: React.FC = () => {
   return (
     <>
       <div className="container mx-auto my-auto text-[#242424] md:text-center py-8 mt-10 ">
-        <h1 className="text-3xl font-semibold mb-4">VST Plugins</h1>
+        <h1 className="head-text_1 mb-4">VST Plugins</h1>
         <div className="max-w-3xl mx-auto">
-          <p className="text-md">
+          <p className="paragraph-text">
             Discover the tools to elevate your music production to new heights.
             Whether you're sculpting lush synth textures, shaping dynamic drum
             beats, or adding the perfect finishing touch with audio effects, our
