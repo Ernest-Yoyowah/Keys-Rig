f784e044a27b966342d230ec4624ba0c3c7c23ce
ernkeyz <exconzy26@gmail.com>
2024-04-23 09:56:46 +0000

    updating metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About/about";
 import Footer from "@/components/Footer/footer";
 import Header from "@/components/Header/header";
+import Hero from "@/components/Hero/hero";
 import Introductory from "@/components/Introductory/introductory";
 import { Navbar } from "@/components/Navigation/navbar";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between">
         <Navbar />
         <Header />
+        {/* <Hero /> */}
         <Introductory />
         <About />
       </main>
