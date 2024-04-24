81d4a3084ba15e2ea1584556d63bd3584a5b91d9
ernkeyz <exconzy26@gmail.com>
2024-04-24 16:53:06 +0000

    Loading UI and Streaming implementation

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <main className="text-center">
+    <main className="bg-gray-800 py-12 text-center text-white">
       <h1>loading....</h1>
     </main>
   );
