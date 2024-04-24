67db025cee602250dac6f4730fb3cf23131d168d
ernkeyz <exconzy26@gmail.com>
2024-04-24 17:51:00 +0000

    Loading UI and Streaming implementation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Hero from "@/components/Hero/hero";
 import Introductory from "@/components/Introductory/introductory";
 import { Navbar } from "@/components/Navigation/navbar";
 import "../styles/globals.css";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function Home() {
   return (
