84b41520f17a2431d8ea4de7192fa8dbce5eb08b
ernkeyz <exconzy26@gmail.com>
2024-04-26 19:31:49 +0000

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <div className="bg-gray-800 py-12 text-center text-white">
         <div className="container mx-auto">
           <h1 className="hero-text_2">Not Found</h1>
-          <p className="mt-4 sub-hero-text_2">
+          <p className="my-4 sub-hero-text_2">
             Could not find requested resource
           </p>
           <Button>
