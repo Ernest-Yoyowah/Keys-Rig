295baa097ac55d09a7ad38b2dd16fd81f223bc71
ernkeyz <exconzy26@gmail.com>
2024-04-26 19:23:11 +0000

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex min-h-screen items-center">
+    <main className="flex min-h-screen flex-col items-center">
       <Navbar />
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
