0e3db35de643d9439c7297c91a3a492788c4ac4f
ernkeyz <exconzy26@gmail.com>
2024-04-26 19:27:26 +0000

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,15 @@ export default function NotFound() {
   return (
     <main className="flex flex-col items-center">
       <Navbar />
-      <div className="py-20">
-        <h2>Not Found</h2>
-        <p>Could not find requested resource</p>
-        <Link href="/">Return Home</Link>
+
+      <div className="bg-gray-800 py-12 text-center text-white">
+        <div className="container mx-auto">
+          <h1 className="hero-text_2">Not Found</h1>
+          <p className="mt-4 sub-hero-text_2">
+            Could not find requested resource
+          </p>
+          <Link href="/">Return Home</Link>
+        </div>
       </div>
     </main>
   );
