1d642b15b9869c32d46c4f077642a48a8a818114
ernkeyz <exconzy26@gmail.com>
2024-04-26 19:22:47 +0000

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,13 @@
+import { Navbar } from "@/components/Navigation/navbar";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
+    <main className="flex min-h-screen items-center">
+      <Navbar />
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
-    </div>
+    </main>
   );
 }
