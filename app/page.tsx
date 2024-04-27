7dd05435da92647182257574a1b5190108bd6412
ernkeyz <exconzy26@gmail.com>
2024-04-27 13:42:01 +0000

    updating the main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "../styles/globals.css";
 import { Suspense } from "react";
 import Loading from "./blog/loading";
 import AboutPage from "@/components/About/AboutPage";
+import FeatureSection from "@/components/ui/FeatureSection";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Introductory />
         <AboutPage />
         <About />
+        <FeatureSection />
       </main>
     </>
   );
