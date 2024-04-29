147c61a68875e7a2bc34972ad22561f931177561
ernkeyz <exconzy26@gmail.com>
2024-04-29 14:38:40 +0000

    updating the blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,88 +2,13 @@ import React, { Suspense } from "react";
 import { NextPage } from "next";
 import { Metadata } from "next";
 import Loading from "./loading";
+import { largeBlogData } from "../lib/blogData";
+import { smallBlogData } from "../lib/blogData";
 
 export const metadata: Metadata = {
   title:
     "Blog - Explore insightful articles and resources about keyboard rigs and music production on Keys Rig.",
 };
-
-// Define an array to store large blog data
-const largeBlogData = [
-  {
-    title: "Large Blog Post Title 1",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the large blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet lorem eu mauris fermentum malesuada. Aliquam posuere auctor justo, ac sagittis enim interdum in.",
-    image:
-      "https://media.gettyimages.com/id/1352603244/photo/shot-of-an-unrecognizable-businessman-working-on-his-laptop-in-the-office.jpg?s=612x612&w=gi&k=20&c=P5z85b4Wg80UO19u2YvRAbmpwxqWyyImihiMb6gWp8I=",
-  },
-  {
-    title: "Large Blog Post Title 2",
-    author: "Ernest Nii",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the large blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet lorem eu mauris fermentum malesuada. Aliquam posuere auctor justo, ac sagittis enim interdum in.",
-    image: "https://wemakestuff.co/wp-content/uploads/2018/05/coffee.jpg",
-  },
-];
-
-const smallBlogData = [
-  {
-    title: "Small Blog Post Title",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image:
-      "https://github.blog/wp-content/uploads/2024/04/keyboard-shortcuts.png?resize=800%2C425",
-  },
-  {
-    title: "Small Blog Post Title",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image:
-      "https://www.searchenginejournal.com/wp-content/uploads/2020/08/7-ways-a-blog-can-help-your-business-right-now-5f3c06b9eb24e-1280x720.png",
-  },
-  {
-    title: "Small Blog Post Title",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "https://ghost.org/images/marketplace/themes/gallery/16.png",
-  },
-  {
-    title: "Small Blog Post Title",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image:
-      "https://lh3.googleusercontent.com/proxy/czTEIp7HXPeeruxP9_eJDSY7wjThNiB3fyRVKC6qANTxFhRUh6q9HXY5rF46kEdJByA94qvrACPPE7RJxJsbczg8BLQrv_zWHJrT03HvPZ12Wx8R-oDO1JaIeUgc6e_aBK2_",
-  },
-  {
-    title: "Small Blog Post Title",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image:
-      "https://www.puppet.com/sites/default/files/image/2024-04/image-pup-blog-migrate-os.jpg",
-  },
-  {
-    title: "Small Blog Post Title",
-    author: "John Doe",
-    date: "April 22, 2024",
-    excerpt:
-      "This is an example blog post excerpt for the small blog posts. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image:
-      "https://img.freepik.com/premium-photo/social-media-blog-concept-futuristic-icon-design-graphics-hand-with-smartphone_102583-6104.jpg",
-  },
-];
 
 const BlogPage: NextPage = () => {
   return (
