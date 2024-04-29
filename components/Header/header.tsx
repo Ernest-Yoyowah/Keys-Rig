516f0ec1ccd2e9a19d25214fefdeba1e4f93111e
ernkeyz <exconzy26@gmail.com>
2024-04-29 23:00:34 +0000

    update

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -3,14 +3,17 @@ import AppBgImg from "./AppBgImg";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
+const content = {
+  headText: "Welcome to Keys Rig",
+  paragraphText: "Step into a world where creativity knows no bounds...",
+};
+
 const Header: React.FC = () => {
   return (
     <div className="flex justify-center items-center mb-0">
       <div className="absolute text-center mx-3 mt-0">
-        <h1 className="hero-text">Welcome to Keys Rig</h1>
-        <p className="sub-hero-text m-2">
-          Step into a world where creativity knows no bounds...
-        </p>
+        <h1 className="hero-text">{content.headText}</h1>
+        <p className="sub-hero-text m-2">{content.paragraphText}</p>
         <br />
         <div className="space-x-2">
           <Button
