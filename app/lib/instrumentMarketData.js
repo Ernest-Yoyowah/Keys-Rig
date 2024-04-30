4dc118dfd58224566b23012cdfc9c8d680cd2956
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-04-30 23:21:16 +0000

    update

---
+++ app/lib/instrumentMarketData.js
@@ -0,0 +1,26 @@
+export const marketSections = [
+  {
+    title: "Rentals",
+    description:
+      "Looking to rent instruments or equipment for your gigs or events? Browse our rental listings for a wide range of musical gear.",
+    imageUrl:
+      "https://www.thomann.de/blog/wp-content/uploads/2023/07/HelloNewGear_header_770x425_july2023.png",
+    link: "/hub/instrumentMarket/repair-rental",
+  },
+  {
+    title: "Sound Engineering Services",
+    description:
+      "Need professional sound engineering services for your recordings or live performances? Discover experienced sound engineers who can help you achieve the perfect sound.",
+    imageUrl:
+      "https://admi.ac.ke/wp-content/uploads/2023/09/Untitled-design-45.png",
+    link: "/hub/instrumentMarket/sound-engineering",
+  },
+  {
+    title: "VST and Music Tech Setup",
+    description:
+      "Explore VST plugins, software, and music tech setup services to enhance your music production setup. Find the latest technology and expert assistance to take your music to the next level.",
+    imageUrl:
+      "https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2023/07/fi-from-arturia.jpg",
+    link: "/hub/instrumentMarket/meet-instrumentalists",
+  },
+];
