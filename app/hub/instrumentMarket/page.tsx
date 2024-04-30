a2bbce12d0b5271087395f2b552a34a30d9f1c61
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-04-30 23:34:28 +0000

    update

--- app/hub/instrumentMarket/page.tsx
+++ app/hub/instrumentMarket/page.tsx
@@ -2,33 +2,10 @@ import React from "react";
 import { NextPage } from "next";
 import Link from "next/link";
 import InstrumentMarketSection from "@/components/ui/InstrumentMarketSection";
-
-const sections = [
-  {
-    title: "Rentals",
-    description:
-      "Looking to rent instruments or equipment for your gigs or events? Browse our rental listings for a wide range of musical gear.",
-    imageUrl:
-      "https://www.thomann.de/blog/wp-content/uploads/2023/07/HelloNewGear_header_770x425_july2023.png",
-    link: "/hub/instrumentMarket/repair-rental",
-  },
-  {
-    title: "Sound Engineering Services",
-    description:
-      "Need professional sound engineering services for your recordings or live performances? Discover experienced sound engineers who can help you achieve the perfect sound.",
-    imageUrl:
-      "https://admi.ac.ke/wp-content/uploads/2023/09/Untitled-design-45.png",
-    link: "/hub/instrumentMarket/sound-engineering",
-  },
-  {
-    title: "VST and Music Tech Setup",
-    description:
-      "Explore VST plugins, software, and music tech setup services to enhance your music production setup. Find the latest technology and expert assistance to take your music to the next level.",
-    imageUrl:
-      "https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2023/07/fi-from-arturia.jpg",
-    link: "/hub/instrumentMarket/meet-instrumentalists",
-  },
-];
+import {
+  marketSections,
+  marketTextContent,
+} from "@/app/lib/instrumentMarketData";
 
 const InstrumentMarket: NextPage = () => {
   return (
@@ -46,19 +23,14 @@ const InstrumentMarket: NextPage = () => {
         {/* Content */}
         <div className="absolute inset-0 flex items-center justify-center text-white">
           <div className="text-center max-w-4xl md:p-0 px-5">
-            <h1 className="hero-text_2 mb-4">Instrument Market</h1>
-            <p className="sub-hero-text_2">
-              Welcome to our instrument market! Browse through listings to buy
-              or sell musical instruments and equipment. Connect with other
-              musicians, collaborate on projects, and find your next favorite
-              gear!
-            </p>
+            <h1 className="hero-text_2 mb-4">{marketTextContent.headText}</h1>
+            <p className="sub-hero-text_2">{marketTextContent.subHeadText}</p>
           </div>
         </div>
       </div>
 
       <div className="container mx-auto py-8 max-w-3xl">
-        {sections.map((section, index) => (
+        {marketSections.map((section, index) => (
           <InstrumentMarketSection key={index} {...section} />
         ))}
       </div>
