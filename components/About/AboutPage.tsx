9cddae4111ece1d20b17fcb61f6a32d740afe8bf
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-04-30 22:22:45 +0000

    update

--- components/About/AboutPage.tsx
+++ components/About/AboutPage.tsx
@@ -7,7 +7,7 @@ const AboutPage: React.FC = () => {
         <div className="w-16 h-0.5 bg-yellow-400 mr-4"></div>
         <p className="text-sm font-normal">Why Do Musicians Love VSTs</p>
       </div>
-      <div className="md:min-h-screen flex py-7 md:w-full">
+      <div className="md:min-h-screen flex py-7 w-full">
         <div className="flex-1 bg-black text-white p-8 flex flex-col justify-center skew-left">
           <h1 className="text-4xl font-bold mb-4 text-center">About VSTs</h1>
           <p className="text-lg mb-8 text-center">
@@ -23,7 +23,7 @@ const AboutPage: React.FC = () => {
             </button>
           </div>
         </div>
-        <div className="flex-1 relative skew-right">
+        <div className="flex-1 relative skew-right md:flex hidden">
           <div className="absolute inset-0 bg-black opacity-20"></div>
           <img
             src="https://png.pngtree.com/png-clipart/20220719/original/pngtree-recording-studio-isometric-music-sound-png-image_8369287.png"
