302f6de578700afbf99937c7e6f94b1af01a1015
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-01 23:55:07 +0000

    update

--- components/About/AboutPage.tsx
+++ components/About/AboutPage.tsx
@@ -27,7 +27,7 @@ const AboutPage: React.FC = () => {
               <Link href="/hub/plugins">VST Plugins</Link>
             </button>
             <button className="bg-white text-black px-4 py-2 rounded-md hover:bg-[#036aa1d2] hover:text-white hover:border-white">
-              <Link href="/hub/plugins">Gear</Link>
+              <Link href="/hub/keysrig/gear">Gear</Link>
             </button>
           </div>
         </div>
