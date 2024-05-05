e43bb742de16d0975f2ef76832e9ab6d6773064b
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-05 20:16:08 +0000

    update

--- app/hub/instrumentMarket/meet-instrumentalists/page.tsx
+++ app/hub/instrumentMarket/meet-instrumentalists/page.tsx
@@ -1,58 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import React from "react";
-
-// Sample data of instrumentalists
-const instrumentalists = [
-  {
-    id: 1,
-    name: "John Doe",
-    instrument: "Guitar",
-    genre: "Rock",
-    imageUrl:
-      "https://www.shutterstock.com/image-photo/smiling-african-american-millennial-businessman-260nw-1437938108.jpg",
-    socialMedia: {
-      twitter: "https://twitter.com/johndoe",
-      instagram: "https://instagram.com/johndoe",
-    },
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    instrument: "Violin",
-    genre: "Classical",
-    imageUrl:
-      "https://t3.ftcdn.net/jpg/02/86/33/20/360_F_286332024_RwH5zhETkoL2LC3p3yuxqvV0l0H32m2J.jpg",
-    socialMedia: {
-      twitter: "https://twitter.com/janesmith",
-      instagram: "https://instagram.com/janesmith",
-    },
-  },
-  {
-    id: 3,
-    name: "Ernest Nii",
-    instrument: "Keyboard",
-    genre: "Classical",
-    imageUrl:
-      "https://pbs.twimg.com/profile_images/1644130887749992448/1l0Fqur7_200x200.jpg",
-    socialMedia: {
-      twitter: "https://twitter.com/ernestnii",
-      instagram: "https://instagram.com/ernestnii",
-    },
-  },
-  {
-    id: 4,
-    name: "Linda Johnson",
-    instrument: "Saxophone",
-    genre: "Jazz",
-    imageUrl:
-      "https://www.shutterstock.com/image-photo/cheerful-african-american-guy-smiling-600nw-2029465811.jpg",
-    socialMedia: {
-      twitter: "https://twitter.com/lindajohnson",
-      instagram: "https://instagram.com/lindajohnson",
-    },
-  },
-];
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  instrumentalists,
+  instrumentalistsTextContent,
+} from "@/app/lib/meetInstrumentalists";
 
 const MeetInstrumentalist: React.FC = () => {
   return (
@@ -68,7 +20,7 @@ const MeetInstrumentalist: React.FC = () => {
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="container mx-auto px-4 relative flex justify-center items-center">
           <h1 className="text-white text-3xl font-semibold">
-            Meet Instrumentalist
+            {instrumentalistsTextContent.headText}
           </h1>
         </div>
       </header>
@@ -80,9 +32,7 @@ const MeetInstrumentalist: React.FC = () => {
             Explore talented instrumentalists
           </h1>
           <p className="text-gray-700 mb-8 ">
-            Connect with talented musicians for gigs, collaborations, and more.
-            Whether you're looking to hire a musician for your event or want to
-            collaborate on a project, you'll find the perfect match here.
+            {instrumentalistsTextContent.paragraphText}
           </p>
         </div>
         <img
