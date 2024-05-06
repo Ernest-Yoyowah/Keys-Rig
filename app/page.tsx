880fbfb48fbbce2cd0709bdc21864460608acb02
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-06 23:52:03 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between">
         <Navbar />
         <Header />
-        {/* <Hero /> */}
         <Introductory />
         <AboutPage />
         <About />
