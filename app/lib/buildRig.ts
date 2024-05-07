843da98591ac78a82e11ad779b02d2e92a90bd9a
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-07 22:43:20 +0000

    update

--- app/lib/buildRig.ts
+++ app/lib/buildRig.ts
@@ -5,5 +5,7 @@ export const buildRigTextContent = {
   paragraphText_1:
     "One of the hardest parts of being a worship keys player is choosing which gear to buy when building your keyboard rig. Knowing what keys gear is right for you and how it works together will benefit your playing, as well as the entire sound of the band!",
   paragraphText_2:
+    "In this 2 part video series, Keith Duell breaks down his keyboard rig in full detail!",
+  paragraphText_3:
     "Keith has been performing in live worship bands and in the studio as a keyboardist for many years. In this series, he will break down all of the hardware and software required to achieve full, rich keys and synth sounds. Watch now!",
 };
