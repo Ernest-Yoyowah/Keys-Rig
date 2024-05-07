5f726ca8faff42d47f3ee04147d154c8cbcf21c0
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-07 22:38:08 +0000

    update

---
+++ app/lib/buildRig.ts
@@ -0,0 +1,9 @@
+export const buildRigTextContent = {
+  headText: "Virtual Worship Rig",
+  subHeadText:
+    "One of the hardest parts of being a worship keys player is choosing which gear to buy when building your keyboard rig.",
+  paragraphText_1:
+    "One of the hardest parts of being a worship keys player is choosing which gear to buy when building your keyboard rig. Knowing what keys gear is right for you and how it works together will benefit your playing, as well as the entire sound of the band!",
+  paragraphText_2:
+    "Keith has been performing in live worship bands and in the studio as a keyboardist for many years. In this series, he will break down all of the hardware and software required to achieve full, rich keys and synth sounds. Watch now!",
+};
