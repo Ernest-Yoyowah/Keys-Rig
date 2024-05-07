10bcfdabf1277737bb6f9365698d18b9be997464
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-07 22:39:21 +0000

    update

--- app/hub/keysrig/build-rig/page.tsx
+++ app/hub/keysrig/build-rig/page.tsx
@@ -18,7 +18,7 @@ const BuildRigPage = () => {
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex items-center justify-center text-white">
           <div className="text-center max-w-4xl md:p-0 px-5">
-            <h1 className="hero-text_2 mb-4">Virtual Worship Rig</h1>
+            <h1 className="hero-text_2 mb-4">{buildRigTextContent.headText}</h1>
             <p className="sub-hero-text_2">
               One of the hardest parts of being a worship keys player is
               choosing which gear to buy when building your keyboard rig.
