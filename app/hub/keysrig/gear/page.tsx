df011ba8704ac985f8bcc0655319021b2265798a
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-07 22:28:35 +0000

    update

--- app/hub/keysrig/gear/page.tsx
+++ app/hub/keysrig/gear/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
-import { gearItems } from "@/app/lib/gearItems";
+import { gearItems, gearTextContent } from "@/app/lib/gearItems";
 
 export const metadata: Metadata = {
   title:
