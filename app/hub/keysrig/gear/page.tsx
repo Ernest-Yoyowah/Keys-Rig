ea9eba3c6b28fb459c7582e437ed8d27faf5dc3f
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-07 22:26:47 +0000

    update

--- app/hub/keysrig/gear/page.tsx
+++ app/hub/keysrig/gear/page.tsx
@@ -1,73 +1,11 @@
 import React from "react";
 import { Metadata } from "next";
+import { gearItems } from "@/app/lib/gearItems";
 
 export const metadata: Metadata = {
   title:
     "Gear - Choosing which gear to buy when building your keyboard rig can be overwhelming",
 };
-
-const gearItems = [
-  {
-    name: "Laptop",
-    image:
-      "https://a.storyblok.com/f/152284/1080x720/186635879f/m2-macbook-air.png",
-  },
-  {
-    name: "Keyboard",
-    image:
-      "https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-81228233/81228233.jpg",
-  },
-  {
-    name: "Keyboard Stand",
-    image:
-      "https://americansongwriter.com/wp-content/uploads/2023/01/midi-stands.jpg?resize=300%2C300",
-  },
-  {
-    name: "MIDI Controller",
-    image:
-      "https://www.native-instruments.com/typo3temp/pics/img-ce-seamless-crop-right-seo-keyboards-landing-page-03-a-series-1a48b1a58237ffed9187a9dc719660bc-m@2x.jpg",
-  },
-  {
-    name: "Laptop Stand",
-    image:
-      "https://headliner-la.com/wp-content/uploads/2022/03/Digistand-Pro-Header-Image-1940x900-1.jpg",
-  },
-  {
-    name: "Audio Interface",
-    image:
-      "https://splice.com/blog/wp-content/uploads/2023/02/0223-Best-audio-interfaces-in-2023-BlogPost.jpg",
-  },
-  {
-    name: "Nano Controller",
-    image:
-      "https://preview.free3d.com/img/2013/02/2145880837097261029/uw93fgil.jpg",
-  },
-  {
-    name: "Sustain Pedal",
-    image:
-      "https://www-konga-com-res.cloudinary.com/w_400,f_auto,fl_lossy,dpr_3.0,q_auto/media/catalog/product/C/h/Cherub-Keyboard-Sustain-Pedal---Piano-Style-8080273_1.jpg",
-  },
-  {
-    name: "Headphones",
-    image:
-      "https://www.magneticmag.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTk3MDg2NTUyNTAzOTUyNDY3/1.jpg",
-  },
-  {
-    name: "USB Hubs",
-    image:
-      "https://cdn.shopify.com/s/files/1/0552/4206/4048/files/ankerhub2_480x480.jpg?v=1654881487",
-  },
-  {
-    name: "MIDI Cables",
-    image:
-      "https://media.karousell.com/media/photos/products/2020/6/24/usb_midi_interface_cable__digi_1592999718_8a47d99d_progressive",
-  },
-  {
-    name: "Additional Cables",
-    image:
-      "https://cdn-gajed.nitrocdn.com/xnFRynGphiHZVmaFQUenqaOUuQKzUTfS/assets/images/optimized/rev-72f06e6/cdn.digitaldjtips.com/app/uploads/2017/06/09132731/cables-back-digital-dj-tips-1204x642.jpg",
-  },
-];
 
 const textContent = {
   title: "Gear & Software",
