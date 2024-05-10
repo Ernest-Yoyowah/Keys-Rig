cb34fe3ee803afe074a7301210de53ab9fbcdedc
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-10 23:20:13 +0000

    update

---
+++ app/lib/rigTourVideos.ts
@@ -0,0 +1,34 @@
+export const videos = [
+  {
+    url: "https://www.youtube.com/embed/neNbVMelMcQ",
+    name: "Worship Rig Tour - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/I4pKBOxosrk",
+    name: "Sunday Keys - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/neNbVMelMcQ",
+    name: "Worship Rig Tour - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/I4pKBOxosrk",
+    name: "Sunday Keys - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/neNbVMelMcQ",
+    name: "Worship Rig Tour - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/I4pKBOxosrk",
+    name: "Sunday Keys - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/neNbVMelMcQ",
+    name: "Worship Rig Tour - Piano & Keys Gear",
+  },
+  {
+    url: "https://www.youtube.com/embed/I4pKBOxosrk",
+    name: "Sunday Keys - Piano & Keys Gear",
+  },
+];
