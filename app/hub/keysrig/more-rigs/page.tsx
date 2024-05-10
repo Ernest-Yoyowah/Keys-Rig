b9d1e406c8698ef1da0193ef77755272b7c1ff67
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-10 23:22:48 +0000

    update

--- app/hub/keysrig/more-rigs/page.tsx
+++ app/hub/keysrig/more-rigs/page.tsx
@@ -7,41 +7,6 @@ export const metadata: Metadata = {
   title: "Other Rigs - view more rigs from other musicians",
 };
 
-// const videos = [
-//   {
-//     url: "https://www.youtube.com/embed/neNbVMelMcQ",
-//     name: "Worship Rig Tour - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/I4pKBOxosrk",
-//     name: "Sunday Keys - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/neNbVMelMcQ",
-//     name: "Worship Rig Tour - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/I4pKBOxosrk",
-//     name: "Sunday Keys - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/neNbVMelMcQ",
-//     name: "Worship Rig Tour - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/I4pKBOxosrk",
-//     name: "Sunday Keys - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/neNbVMelMcQ",
-//     name: "Worship Rig Tour - Piano & Keys Gear",
-//   },
-//   {
-//     url: "https://www.youtube.com/embed/I4pKBOxosrk",
-//     name: "Sunday Keys - Piano & Keys Gear",
-//   },
-// ];
-
 const MoreRigPage = () => {
   return (
     <div>
