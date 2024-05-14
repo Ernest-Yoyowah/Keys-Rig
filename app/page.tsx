f0ef738257c78b22ff52cf81036e6cfb6b05806c
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-14 16:33:39 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Header from "@/components/Header/header";
 import Introductory from "@/components/Introductory/introductory";
 import { Navbar } from "@/components/Navigation/navbar";
 import "../styles/globals.css";
-import { Suspense } from "react";
-import Loading from "./blog/loading";
 import AboutPage from "@/containers/home-page/About/AboutPage";
 import FeatureSection from "@/components/ui/FeatureSection";
 import ArticlePage from "@/containers/home-page/About/article";
