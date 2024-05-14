11c86aba23945edfaed6a3ba217d2204ba9bbcd6
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-14 16:40:35 +0000

    updating folder structure

---
+++ containers/home-page/Header/AppBgImg.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import bgImage from "@/public/header_bg2.png";
+
+const AppBgImg: React.FC = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+      <Image
+        src={bgImage}
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+          zIndex: -1,
+          filter: "brightness(60%)",
+        }}
+        alt="Header Image"
+      />
+    </div>
+  );
+};
+
+export default AppBgImg;
