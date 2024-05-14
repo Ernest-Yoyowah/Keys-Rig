6b97b5894894b6c732ab59e8cf17f98653cb27e6
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-14 16:43:06 +0000

    updating folder structure

--- components/VstPlugins/vstPlugins.tsx
+++ components/VstPlugins/vstPlugins.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Featured from "../FeaturedRig/featuredRig";
+import Featured from "../../containers/plugins-page/FeaturedRig/featuredRig";
 
 const vstPlugins: React.FC = () => {
   return (
