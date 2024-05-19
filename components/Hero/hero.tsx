4d9de188e52b5833645a13f466d53f60894fba91
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-19 22:48:36 +0000

    Refactoring codebase

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -31,32 +31,11 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="md:mx-auto px-5 my-auto text-slate-800 md:text-center py-8">
-        <div className="max-w-3xl mx-auto">
-          <p>
-            One of the hardest parts of being a worship keys player is choosing
-            which gear to buy when building your keyboard rig. Knowing what keys
-            gear is right for you and how it works together will benefit your
-            playing, as well as the entire sound of the band!
-          </p>
-          <br />
-          <p>
-            In this 2 part video series, Keith Duell breaks down his keyboard
-            rig in full detail!
-          </p>
-          <div>
-            <YoutubeEmbed
-              videoId="neNbVMelMcQ"
-              title="Keys Gear | Sunday Keys App Setup"
-            />
-          </div>
-          <p>
-            Keith has been performing in live worship bands and in the studio as
-            a keyboardist for many years. In this series, he will break down all
-            of the hardware and software required to achieve full, rich keys and
-            synth sounds. Watch now!
-          </p>
-        </div>
+      <div>
+        <YoutubeEmbed
+          videoId="neNbVMelMcQ"
+          title="Keys Gear | Sunday Keys App Setup"
+        />
       </div>
     </>
   );
