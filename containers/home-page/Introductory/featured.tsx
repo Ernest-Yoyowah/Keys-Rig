59e59e14df8985539d71cd512706e7ea0b75fdca
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-05-21 22:39:47 +0000

    updating file and folder structure

---
+++ containers/home-page/Introductory/featured.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Logo from "../../../components/ui/Logo";
+
+const logos = [
+  {
+    src: "/ableton_logo.jpg",
+    alt: "Logo 1",
+    heightClass: "h-12",
+    width: 100,
+    height: 200,
+  },
+  {
+    src: "/FL-Studio-Hero.png",
+    alt: "Logo 2",
+    heightClass: "h-14",
+    width: 130,
+    height: 50,
+  },
+  {
+    src: "/korg.jpg",
+    alt: "Logo 3",
+    heightClass: "h-12",
+    width: 100,
+    height: 70,
+  },
+];
+
+const Featured: React.FC = () => {
+  return (
+    <div className="bg-gray-100 py-8 mt-0">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-semibold mb-4 text-zinc-700 font-mono">
+          FEATURED IN
+        </h2>
+        <div className="flex md:flex-wrap justify-center items-center gap-2 md:space-x-8">
+          {logos.map((logo, index) => (
+            <Logo
+              key={index}
+              src={logo.src}
+              alt={logo.alt}
+              heightClass={logo.heightClass}
+              width={logo.width}
+              height={logo.height}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
