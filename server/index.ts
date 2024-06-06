4f5a52843611b675aaad0eeabdfdea8c18144ea1
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-06-06 20:31:05 +0000

    setting up server

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import next from "next";
+import connectMongoDB from "./mongodb";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+
+app.prepare().then(() => {
+  // Connect to MongoDB
+  connectMongoDB();
+
+  // Middleware to handle Next.js requests
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  // Start the server
+  server.listen(PORT, (err?: any) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
