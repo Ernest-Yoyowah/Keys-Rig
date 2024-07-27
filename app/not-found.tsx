aa91ededa3b9c8f5c8bc421bcc760d40a789d3c6
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-07-27 23:36:16 +0000

    page 404 UI update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,9 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <>
-      <Navbar />
       <div className="bg-gray-800 py-12 text-center text-white">
-        <h1 className="text-3xl font-semibold">Not Found</h1>
         <div className="container mx-auto">
+          <h1 className="text-3xl font-semibold">Not Found</h1>
           <p className="my-4 sub-hero-text_2">
             Could not find requested resource
           </p>
