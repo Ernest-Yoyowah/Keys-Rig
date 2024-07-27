9563fbf317b19e8552ccf6dcb6679c31f2bd0900
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-07-27 23:40:18 +0000

    page 404 UI update

--- app/hub/layout.tsx
+++ app/hub/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/Navigation/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="">
+    <div>
       <div>
         <Navbar />
       </div>
