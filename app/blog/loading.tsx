65b8537450ed0d6775d9be36f917aed949943786
Ernest Yoyowah <117729383+Ernest-Yoyowah@users.noreply.github.com>
2024-09-06 23:24:32 +0000

    Update loading.tsx

--- app/blog/loading.tsx
+++ app/blog/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="bg-gray-800 py-12 text-center text-white">
-      <h1 className="hero-text_2">Loadgin...</h1>
+      <h1 className="hero-text_2">Loading...</h1>
     </div>
   );
 };
